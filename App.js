ab7fbfd847a231f99865a24fa21ba39f4a35b2a7
timbaron <timothy.akiode@student.aul.edu.ng>
2022-04-28 13:17:35 +0100

    TextImput, Text and view components

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [name,setName ] = useState('Timothy');
+  const [age, setAge] = useState(21);
+
+  const clickHandler = () => {
+    setName('Samuel');
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter Your Name</Text>
+      <TextInput
+        multiline
+       style={styles.input} 
+        placeholder="John Joe"
+        onChangeText={(val) => setName(val)}
+       />
+       <Text>Enter yout age</Text>
+       <TextInput 
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder="21"
+        onChangeText={(age) => setAge(age)}
+       />
+      <Text>My name is {name} , age: {age}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +38,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    width: 200,
+    height: 40,
+    margin: 10,
+    padding: 8,
+  }
 });
